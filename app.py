91df290c9c88ea2f9e089099b08c9228ee042af3
soumenweb <soumenclik700@gmail.com>
2025-06-09 09:46:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,38 +19,6 @@ hide_st_style = """
    </style>
    """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-hide_streamlit_style = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stStatusWidget"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                #MainMenu {
-                visibility: hidden;
-                height: 0%;
-                }
-                header {
-                visibility: hidden;
-                height: 0%;
-                }
-                footer {
-                visibility: hidden;
-                height: 0%;
-                }
-                </style>
-                """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 init_db()
 
